e9d413cb8b41e26d6000c246fe8b3f393beb7777
Nguyen Thanh <nguyen.thanh@alliedtechcamp.com>
2019-03-05 11:44:21 +0700

    update source

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->call('\App\Http\Controllers\Auth\IndexController@getTrends')->cron('* * * * *');
+        $schedule->call('\App\Http\Controllers\IndexController@getTrends')->hourly();
     }
 
     /**
